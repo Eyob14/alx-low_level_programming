8ec4cdd0fe9935368bbb3e2054c85d036ae41bd0
Eyob14 <eyobzebene3580@gmial.com>
2022-03-30 03:02:50 -0700

    100-print_comb3.c added

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != 56)
+				{
+					putchar(44);
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
