bcebada9c7bd90f71f56d2fdda39ab4a1494f39e
Eyob14 <eyobzebene3580@gmial.com>
2022-03-31 06:29:35 -0700

    4-isalpha.c added

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entery point
+ * @c: character to check
+ *
+ * Description: checks for alphabetic character
+ *
+ * Return: 0 or 1
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
