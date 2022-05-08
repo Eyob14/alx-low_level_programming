1f8b4d487a3b69e951081d8480aaf878f480dcd8
Eyob14 <Eyobzebene3580@gmail.com>
2022-05-08 06:55:07 -0700

    2-mul.c added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *
+ * @a: an integer input
+ * @b: an integer input
+ *
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
