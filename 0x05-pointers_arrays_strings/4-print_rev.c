e60fd5b4edcfcd89fa2a59874b6a3126174da21a
Eyob14 <Eyobzebene3580@gmail.com>
2022-05-16 07:25:03 -0700

    4-print_rev.c updated

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,20 +9,30 @@
  */
 void print_rev(char *s)
 {
-	int length = 0;
-	char c = *(s + length);
+	int i = 0;
+
+	for (i = (_strlen(s) - 1); i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
+
+/**
+ * _strlen - is a function that returns the length of a string
+ *
+ * @s: a string parameter
+ *
+ *
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+	int length = 1;
+	char c = s[0];
 
 	while (c != '\0')
 	{
-		length++;
 		c = *(s + length);
+		++length;
 	}
-
-	while (length >= 0)
-	{
-		_putchar(c);
-		--length;
-		c = *(s + length);
-	}
-	_putchar('\n');
+	return (length - 1);
 }
